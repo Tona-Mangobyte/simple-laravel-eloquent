4131ae3712fc80a4b4961531648938d60268b2ee
Tona <tona@mango-byte.com>
2022-10-23 10:35:40 +0700

    add audit article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,16 +2,35 @@
 
 namespace App\Models;
 
+use App\Events\ArticleSaved;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Log;
 
 class Article extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
 
     protected $fillable = ['user_id', 'title', 'content'];
 
     protected $casts = [
         'user_id' => 'integer'
     ];
+
+    /*protected $dispatchesEvents = [
+        'saved' => ArticleSaved::class,
+    ];*/
+
+    /**
+     * The "booted" method of the model.
+     *
+     * @return void
+     */
+    /*protected static function booted()
+    {
+        static::created(function ($article) {
+            Log::info("article create is success ". $article->id);
+        });
+    }*/
 }
---
+++ app/Observers/ArticleObserver.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Article;
+use Illuminate\Support\Facades\Log;
+
+class ArticleObserver
+{
+    /**
+     * Handle the Article "created" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function created(Article $article)
+    {
+        Log::info("article create is success ". $article->id);
+    }
+
+    /**
+     * Handle the Article "updated" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function updated(Article $article)
+    {
+        Log::info("article update is success ". $article->id);
+    }
+
+    /**
+     * Handle the Article "deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function deleted(Article $article)
+    {
+        Log::info("article delete is success ". $article->id);
+    }
+
+    /**
+     * Handle the Article "restored" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function restored(Article $article)
+    {
+        Log::info("article restored is success ". $article->id);
+    }
+
+    /**
+     * Handle the Article "force deleted" event.
+     *
+     * @param  \App\Models\Article  $article
+     * @return void
+     */
+    public function forceDeleted(Article $article)
+    {
+        Log::info("article forceDeleted is success ". $article->id);
+    }
+}
